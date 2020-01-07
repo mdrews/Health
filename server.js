d44ceb951358932365483859ea0c47363b96ad4c
mdrews <matthewdrews@gmail.com>
2020-01-06 23:23:53 -0500

    add mongo connection to project

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const bodyParser = require('body-parser');
 const chalk = require('chalk');
 const express = require('express');
+const mongoose = require('mongoose');
+
+const config = require('./config')
 
 // Middleware
 const app = express();
 app.use(bodyParser.json());
 
 const port = process.env.port || 5000;
+
+mongoose.connect(config.mongoURI)
+  .then(() => console.log(chalk.magenta(`Connected to MongoDB`)))
+  .catch(error => console.log(chalk.red(`Error connecting to MongoDB: ${error}`)));
 
 app.listen(port, () => {
   console.log(`Server alive and listening on port: ${chalk.blue(port)}`);
