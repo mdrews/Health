836ad1480728ffe16334bbe42edbf4be5d175ace
mdrews <matthewdrews@gmail.com>
2020-01-08 23:57:20 -0500

    add weight post route

--- routes/api/src/weight.js
+++ routes/api/src/weight.js
@@ -8,4 +8,12 @@ router.get('/', (req, res) => {
     .then(weights => res.json(weights));
 });
 
+router.post('/', (req, res) => {
+  const newWeight = new Weight({
+    user: req.body.user,
+    weight: req.body.weight
+  });
+  newWeight.save().then(weight => res.json(weight));
+});
+
 module.exports = router;
