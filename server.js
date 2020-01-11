8217381ceb0aa9cc50ccc1263d6ea14d345e8850
mdrews <matthewdrews@gmail.com>
2020-01-10 23:42:55 -0500

    update route for weights

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const config = require('./config');
-const routes = require('./routes/api/weight');
+const weights = require('./routes/api/weight');
 
 // Middleware
 const app = express();
 app.use(bodyParser.json());
-app.use(routes);
+app.use('/api/weights', weights);
 
 const port = process.env.port || 5000;
 
