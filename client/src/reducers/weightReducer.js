c0127e84e32b916ce2114f5ae79bf179966eb691
mdrews <matthewdrews@gmail.com>
2020-02-04 23:52:08 -0500

    return state for add weight reducer

--- client/src/reducers/weightReducer.js
+++ client/src/reducers/weightReducer.js
@@ -1,6 +1,9 @@
+import { ADD_WEIGHT } from "../actions/consts";
 
 export const weightReducer = (state = [], action) => {
   switch(action.type) {
+    case ADD_WEIGHT:
+      return [...state]
     default:
       return state;
   }
