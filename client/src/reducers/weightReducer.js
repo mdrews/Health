9f2a359651dc68c476949a498c45e2eb24499e74
mdrews <matthewdrews@gmail.com>
2020-02-05 23:02:52 -0500

    add new weight to state

--- client/src/reducers/weightReducer.js
+++ client/src/reducers/weightReducer.js
@@ -3,7 +3,7 @@ import { ADD_WEIGHT } from "../actions/consts";
 export const weightReducer = (state = [], action) => {
   switch(action.type) {
     case ADD_WEIGHT:
-      return [...state]
+      return [...state, { date: Date.now(), weight: action.weight }]
     default:
       return state;
   }
