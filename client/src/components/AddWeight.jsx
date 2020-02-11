9cf51062f3fee62ed1ce3b4b5a8e012ecb223186
mdrews <matthewdrews@gmail.com>
2020-02-10 23:54:14 -0500

    update datepicker for weight component

--- client/src/components/AddWeight.jsx
+++ client/src/components/AddWeight.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDatePicker from 'react-datepicker';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
 
 const AddWeight = () => {
+
+  const [startDate, setStartDate] = useState(Date.now());
+
   return(<div>
-    <ReactDatePicker />
+    <DatePicker
+      selected={startDate}
+      onChange={date => setStartDate(date)}
+      showTimeSelect
+      timeFormat="HH:mm"
+      timeIntervals={15}
+      timeCaption="time"
+      dateFormat="MMMM d, yyyy h:mm aa"
+    />
     <button>Add Weight</button>
   </div>);
 }
