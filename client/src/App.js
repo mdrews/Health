a2818ba137d23fed2b6a612d2160957dee4d9ed8
mdrews <matthewdrews@gmail.com>
2020-02-11 23:43:29 -0500

    fix formatting for date picker

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ function App() {
         </a>
         <AddWeight />
       </header>
+      <AddWeight />
     </div>
   );
 }
--- client/src/components/AddWeight.jsx
+++ client/src/components/AddWeight.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 
+import "react-datepicker/dist/react-datepicker.css";
+
 const AddWeight = () => {
 
   const [startDate, setStartDate] = useState(Date.now());
