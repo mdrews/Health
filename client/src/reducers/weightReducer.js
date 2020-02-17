fc590241456172a581f6262a941171fc6f41f013
mdrews <matthewdrews@gmail.com>
2020-02-16 23:41:03 -0500

    add set loading to weight reducer

--- client/src/reducers/weightReducer.js
+++ client/src/reducers/weightReducer.js
@@ -1,10 +1,12 @@
-import { ADD_WEIGHT } from "../actions/consts";
+import { ADD_WEIGHT, SET_LOADING } from "../actions/consts";
 
 export const weightReducer = (state = [], action) => {
   console.log('reducer');
   switch(action.type) {
     case ADD_WEIGHT:
       return [...state, { date: Date.now(), weight: action.weight }]
+    case SET_LOADING:
+      return state;
     default:
       return state;
   }
