7236be4d9b519757931fb8c1d3f26d7f200e79a5
mdrews <matthewdrews@gmail.com>
2020-02-19 23:42:24 -0500

    add comment

--- client/src/reducers/weightReducer.js
+++ client/src/reducers/weightReducer.js
@@ -6,6 +6,7 @@ export const weightReducer = (state = [], action) => {
     case ADD_WEIGHT:
       return [...state, { date: Date.now(), weight: action.weight }]
     case SET_LOADING:
+      // comment
       console.log('updating');
       return state;
     default:
