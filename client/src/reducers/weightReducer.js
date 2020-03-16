e6814f4c485391ea6ddbbfb7451ef8512beab79e
mdrews <matthewdrews@gmail.com>
2020-03-15 23:37:21 -0400

    a the remove_weight action to weight reducer

--- client/src/reducers/weightReducer.js
+++ client/src/reducers/weightReducer.js
@@ -1,4 +1,4 @@
-import { ADD_WEIGHT, SET_LOADING, GET_WEIGHT } from "../actions/consts";
+import { ADD_WEIGHT, SET_LOADING, GET_WEIGHT, REMOVE_WEIGHT } from "../actions/consts";
 
 export const weightReducer = (state = [], action) => {
   console.log('reducer');
@@ -8,6 +8,8 @@ export const weightReducer = (state = [], action) => {
     case GET_WEIGHT:
       console.log('reducer: get_weight');
       return {loading: false, weights: action.weights};
+    case REMOVE_WEIGHT:
+      return { loading: false, weights: action.weights};
     case SET_LOADING:
       console.log('loading...');
       return {...state, loading: true};
