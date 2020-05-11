62bde5cce3477928705a6f205802178f22e1323b
mdrews <matthewdrews@gmail.com>
2020-05-10 23:25:59 -0400

    modify comment

--- models/Weight.js
+++ models/Weight.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-/// Weight Object blah
+/// Weight Object
 const WeightSchema = new Schema({
   user: {
     required: true,
